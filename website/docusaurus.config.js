a7b05d9a71439c76ac0d56180dd1e8d222fade34
Raghav Mecheri <37787004+raghavmecheri@users.noreply.github.com>
2021-02-01 14:43:17 -0500

    Update docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://raghavmecheri.me',
+  baseUrl: '/pnguin/',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
